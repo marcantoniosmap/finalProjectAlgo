d880a1777a4f69a65cc73384c15c612fc70cf162
Amartya Saleh Kadarisman <amartya@Amartyas-MacBook-Pro.local>
2019-10-05 12:07:14 +0700

    fixed parser

---
+++ lexer2.py
@@ -0,0 +1,87 @@
+
+#TO DO NEXT : GROUPING
+
+import ply.lex as lex
+import ply.yacc as yacc
+import  sys
+
+tokens = [
+    'ID',
+    'R_ANGLE',
+    'SIBLING',
+    'MULTIPLY',
+    'L_CURLY',
+    'R_CURLY',
+    'CLIMB',
+    'DOT',
+    'HASH',
+    'INT',
+]
+
+reserved={
+    'div': 'DIV',
+    'span' : 'SPAN',
+    'p' : 'P',
+    'h1' : 'H1',
+    "h2" : "H2",
+    "h3" : "H3",
+    "h4" : "H4",
+    "h5" : "H5",
+    "h6" : "H6",
+    "ul" : "UL",
+    "li" : "LI",
+    "table" : "TABLE",
+    "td" : "TD",
+    "tr" : "TR",
+    "img" : "IMG",
+    "br" : "BR"
+}
+
+
+tokens += reserved.values()
+
+t_ignore=r' '
+
+t_R_ANGLE=r'\>'
+t_SIBLING=r'\+'
+t_MULTIPLY=r'\*'
+t_L_CURLY=r'\{'
+t_R_CURLY=r'\}'
+t_CLIMB=r'\^'
+t_DOT=r'\.'
+t_HASH=r'\#'
+
+def t_ID(t):
+    r'[a-zA-Z][a-zA-Z0-9]*'
+    t.type=reserved.get(t.value,'ID')
+    if t.type == 'ID':
+        return t_error(t)
+    return t
+
+def t_INT(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+def t_error(t):
+    print('ERROR OCCURED ON :',t.lexpos)
+    t.lexer.skip(1)
+
+
+lexer=lex.lex()
+
+# lexer.input("div > div")
+while 1:
+    s=input(">> ")
+    lexer.input(s)
+    while True:
+        tok = lexer.token()
+        if not tok: 
+            break
+        print(tok)
+
+# while True:
+#     s=input(">> ")
+#     lexer.input(s)
+#     tok = lexer.token()
+#     print(tok)
